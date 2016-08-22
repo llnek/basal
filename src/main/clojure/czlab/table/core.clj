76f68153f5f6488f8b27cfadb1c27ce9b168a7af
Kenneth Leung <ccllnekk@gmail.com>
2016-08-22 14:34:59 -0700

    kl

---
+++ src/main/clojure/czlab/table/core.clj
@@ -0,0 +1,120 @@
+;;source from https://github.com/cldwalker/table
+(ns czlab.table.core
+  (:require czlab.table.width)
+  (:use [clojure.string :only [join]] ))
+
+(declare style-for format-cell render-rows-with-fields escape-newline render-rows table-str)
+
+(def ^:dynamic *style* :plain)
+;;(def ^:private walls ["| " " | " " |"])
+(def ^:private walls ["  " "   " "  "])
+(def ^:private styles
+  {
+   :none  {:top ["  " "   " "  "], :middle ["  " "   " "  "] :bottom ["  " "   " "  "]
+           :dash " " :header-walls walls :body-walls walls }
+   :plain {:top ["+-" "-+-" "-+"], :middle ["+-" "-+-" "-+"] :bottom ["+-" "-+-" "-+"]
+           :dash "-" :header-walls walls :body-walls walls }
+   :org {:top ["|-" "-+-" "-|"], :middle ["|-" "-+-" "-|"] :bottom ["|-" "-+-" "-|"]
+         :dash "-" :header-walls walls :body-walls walls }
+   :unicode {:top ["┌─" "─┬─" "─┐"] :middle ["├─" "─┼─" "─┤"] :bottom ["└─" "─┴─" "─┘"]
+             :dash "─" :header-walls ["│ " " │ " " │"] :body-walls ["│ " " ╎ " " │"] }
+   :unicode-3d {:top ["┌─" "─┬─" "─╖"] :middle ["├─" "─┼─" "─╢"] :bottom ["╘═" "═╧═" "═╝"]
+                :top-dash "─" :dash "─" :bottom-dash "═"
+                :header-walls ["│ " " │ " " ║"] :body-walls ["│ " " │ " " ║"] }
+   :github-markdown {:top ["" "" ""] :middle ["|-" " | " "-|"] :bottom ["" "" ""]
+                     :top-dash "" :dash "-" :bottom-dash "" :header-walls walls :body-walls walls }
+   })
+
+(defn table
+   "Generates an ascii table for almost any input that fits in your terminal.
+   Multiple table styles are supported.
+
+   Options:
+
+   * :sort   When set with field name, sorts by field name. When set to true
+             sorts by first column. Default is false.
+   * :fields An optional vector of fields used to control ordering of fields.
+             Only works with rows that are maps.
+   * :desc   When set to true, displays row count after table. Default is nil.
+   * :style  Sets table style. Available styles are :plain, :org, :unicode and
+             :github-markdown. Default is :plain."
+  [& args]
+  (println (apply table-str args)))
+
+(defn table-str
+  "Same options as table but returns table as a string"
+  [ args & {:keys [style] :or {style :plain} :as options}]
+  (binding [*style* (if (map? style) style (style styles))]
+    (apply str (join "\n" (render-rows args (if (map? options) options {}))))))
+
+(defn- generate-rows-and-fields
+  "Returns rows and fields. Rows are a vector of vectors containing string cell values."
+  [table options]
+  (let [
+       top-level-vec (not (coll? (first table)))
+       fields (cond
+               top-level-vec [:value]
+               (map? (first table)) (or (:fields options)
+                                        (distinct (vec (flatten (map keys table)))))
+               (map? table) [:key :value]
+               :else (first table))
+       rows (cond
+             top-level-vec (map #(vector %) table)
+             (map? (first table)) (map #(map (fn [k] (get % k)) fields) table)
+             (map? table) table
+             :else (rest table))
+       rows (map (fn [row] (map #(if (nil? %) "" (str %)) row)) rows)
+       sort-opt (options :sort)
+       rows (if (and sort-opt (some #{sort-opt} (conj fields true)))
+              (sort-by
+               #(nth % (if (true? sort-opt) 0 (.indexOf ^java.util.List fields sort-opt)))
+               rows) rows)
+        rows (->> rows (map vec) (map (fn [row] (map escape-newline row))))]
+    [rows fields]))
+
+(defn- render-rows
+  "Generates a list of formatted string rows given almost any input"
+  [table options]
+  (let [[rows fields] (generate-rows-and-fields table options)
+        rendered-rows (render-rows-with-fields rows fields options)]
+    (if (:desc options)
+      (concat rendered-rows [(format "%s rows in set" (count rows))])
+      rendered-rows)))
+
+(defn- render-rows-with-fields [rows fields options]
+  (let [
+    headers (map #(if (keyword? %) (name %) (str %)) fields)
+    widths (czlab.table.width/get-widths (cons headers rows))
+    fmt-row (fn [row]
+              (map-indexed
+                (fn [idx string] (format-cell string (nth widths idx)))
+                row))
+    wrap-row (fn [row strings] (let [[beg mid end] strings] (str beg (join mid row) end)))
+    headers (fmt-row headers)
+    border-for (fn [section dash]
+                 (let [dash-key (if (style-for dash) dash :dash)]
+                 (wrap-row
+                   (map #(apply str (repeat
+                                      (.length (str %))(style-for dash-key))) headers)
+                   (style-for section))))
+    header (wrap-row headers (style-for :header-walls))
+    body (map #(wrap-row (fmt-row %) (style-for :body-walls)) rows) ]
+
+    (concat [(border-for :top :top-dash) header (border-for :middle :dash)]
+            body [( border-for :bottom :bottom-dash)])))
+
+(defn- escape-newline [string]
+  (clojure.string/replace string (str \newline) (char-escape-string \newline)))
+
+(defn- style-for [k] (k *style*))
+
+(defn format-cell [^String string width]
+  (if (zero? width)
+    ""
+    (format
+      (str "%-" width "." width "s")
+      (if (> (count string) width)
+        (str (.substring string 0 (- width 3)) "...")
+        string))))
+
+
---
+++ src/main/clojure/czlab/table/width.clj
@@ -0,0 +1,75 @@
+;;source from https://github.com/cldwalker/table
+(ns czlab.table.width
+  (:require clojure.java.shell clojure.java.io clojure.string))
+
+(declare get-initial-widths max-width-per-field actual-width auto-resize-widths
+         detect-terminal-width command-exists?)
+
+(def ^:dynamic *width* (delay (or (detect-terminal-width) 200)))
+; TODO: calculate border lengths from styles
+; " | " and "-+-" are inner borders
+(def inner-border-length 3)
+; "+-" and "-+" are outer borders
+(def outer-border-length 2)
+
+(defn get-widths [all-rows]
+  (-> all-rows get-initial-widths vec auto-resize-widths))
+
+(defn auto-resize-widths [widths]
+  (loop [new-widths [] widths widths field-count (count widths) max-width @*width*]
+    (if (empty? widths)
+      new-widths
+      (let [width (first widths)
+            width-per-field (max-width-per-field max-width field-count)
+            new-width (if (< width width-per-field) width width-per-field)]
+        (recur
+          (conj new-widths new-width)
+          (rest widths)
+          (- field-count 1)
+          (- max-width (+ new-width inner-border-length)))))))
+
+(defn get-initial-widths [all-rows]
+  (map
+    (fn [idx]
+      (apply max (map #(count (str (nth % idx))) all-rows)))
+    (range (count (first all-rows)))))
+
+(defn- max-width-per-field [current-width field-count]
+  (quot (actual-width current-width field-count) field-count))
+
+; think of inner-borders as interposed between fields to understand why
+; it's one less than the number of fields
+(defn- actual-width [current-width field-count]
+  (- current-width (+ (* 2 outer-border-length) (* (dec field-count) inner-border-length))))
+
+(defn ensure-valid-width [arg]
+  (if (integer? arg)
+    (if (> arg 0) arg 100)
+    arg))
+(defn- stty-detect [] 80)
+(defn- XXstty-detect []
+  (->> (clojure.java.shell/sh "/bin/sh" "-c" "stty -a < /dev/tty")
+       :out
+       (re-find #" (\d+) columns")
+       vec
+       second
+       ((fn  [_ two] (if two (Integer. (str two)))) :not-used)))
+
+; since Java doesn't recognize COLUMNS by default you need to `export COLUMNS` for it
+; be recognized
+(defn- detect-terminal-width []
+  (ensure-valid-width
+   (cond
+    (System/getenv "COLUMNS") (Integer. (System/getenv "COLUMNS"))
+    (command-exists? "stty") (stty-detect))))
+
+(defn- command-exists?
+  "Determines if command exists in $PATH"
+  [cmd]
+  (some
+    #(-> (str % "/" cmd) clojure.java.io/file .isFile)
+    (-> (System/getenv "PATH") (clojure.string/split #":"))))
+
+
+
+
