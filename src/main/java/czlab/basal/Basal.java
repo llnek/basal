6a4cd57e038f1ef95d71626f8cc06ec81a5f223a
Kenneth Leung <ccllnekk@gmail.com>
2017-01-20 02:53:41 -0800

    kl

--- src/main/java/czlab/basal/Basal.java
+++ src/main/java/czlab/basal/Basal.java
@@ -13,11 +13,7 @@ package czlab.basal;
 /**
  * @author Kenneth Leung
  */
-public class HelloWorld {
-
-  public static void main(String[] args) {
-    System.out.println("hello world!");
-  }
+public class Basal {
 
 }
 
