335557d662f1cbe8f496080995a826310ad35b54
Kenneth Leung <ccllnekk@gmail.com>
2019-11-30 00:03:33 -0800

    added support for cljs

--- project.clj
+++ project.clj
@@ -14,13 +14,27 @@
                  [org.clojure/core.async "0.5.527"]
                  [org.flatland/ordered "1.5.7"]
                  [org.clojure/data.json "0.2.7"]
-                 [org.clojure/tools.logging "0.5.0"]]
+                 [org.clojure/tools.logging "0.5.0"]
+                 [org.clojure/clojurescript "1.10.597"]]
 
   :exclusions [org.clojure/clojure]
 
   :plugins [[cider/cider-nrepl "0.22.4"]
+            [lein-cljsbuild "1.1.7"]
             [lein-codox "0.10.7"]
             [lein-cprint "1.3.2"]]
+
+  :cljsbuild {
+    :builds [{
+        ; The path to the top-level ClojureScript source directory:
+        :source-paths ["src/main/clojure"]
+        ; The standard ClojureScript compiler options:
+        ; (See the ClojureScript compiler documentation for details.)
+        :compiler {
+          ;:output-to "war/javascripts/main.js"
+          ; default: target/cljsbuild-main.js
+          :optimizations :whitespace
+          :pretty-print true}}]}
 
   :profiles {:provided {:dependencies [[org.clojure/clojure
                                         "1.10.1" :scope "provided"]]}
